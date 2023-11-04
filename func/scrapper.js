c4b48df4909176e08ab525fef9292edaebd9ef5b
allefschmidt <dev.allefschmidt@gmail.com>
2023-11-04 19:59:57 -0300

    :sparkles: criada a função getTripData e retirado buildUrl

--- func/scrapper.js
+++ func/scrapper.js
@@ -4,11 +4,13 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
 puppeteer.use(StealthPlugin());
 
-async function getRoundTripData(url) {
+async function getRoundTripData(origem, destino, dataIda, dataVolta) {
     const browser = await puppeteer.launch({
       headless: false, // Abre o navegador em modo não headless para interação manual
     });
   
+    const url = `https://www.skyscanner.com.br/transporte/passagens-aereas/${origem}/${destino}/${dataIda}/${dataVolta}?adultsv2=1&cabinclass=economy&childrenv2=&inboundaltsenabled=false&outboundaltsenabled=false&preferdirects=false&ref=home&rtn=0`;
+
     const page = await browser.newPage();
   
     // Navega para a URL desejada
@@ -50,10 +52,56 @@ async function getRoundTripData(url) {
     // Fechar o navegador após a interação
     await browser.close();
 }
+
+async function getTripData(origem, destino, dataIda) {
+  const browser = await puppeteer.launch({
+    headless: false, // Abre o navegador em modo não headless para interação manual
+  });
+
+  const url = `https://www.skyscanner.com.br/transporte/passagens-aereas/${origem}/${destino}/${dataIda}?adultsv2=1&cabinclass=economy&childrenv2=&inboundaltsenabled=false&outboundaltsenabled=false&preferdirects=false&ref=home&rtn=0`;
+
+  const page = await browser.newPage();
+
+  // Navega para a URL desejada
+  await page.goto(url, { waitUntil: 'networkidle2' });
+
+  // Espera que pelo menos um elemento com a classe desejada esteja presente
+  await page.waitForSelector("div[class^='FlightsTicket_container']", { visible: true });
+
+  // Captura as informações específicas dentro das divs FlightsTicket_container
+  const flightInfo = await page.evaluate(() => {
+    const tickets = Array.from(document.querySelectorAll("div[class^='FlightsTicket_container']"));
+
+    return tickets.map(ticket => {
+      // Extrai informações de partida
+      const departSpans = ticket.querySelectorAll("div[class^='LegInfo_routePartialDepart'] span");
+      const departTime = departSpans[0]?.querySelector('div span')?.textContent.trim() || '';
+      // Usa o seletor correto para o código do aeroporto
+      const departDiv = ticket.querySelector("div[class^='LegInfo_routePartialDepart'] div[aria-label]");
+
+      const departCode = departDiv?.getAttribute('aria-label') || '';
+
+      // Extrai informações de chegada
+      const arriveSpans = ticket.querySelectorAll("div[class^='LegInfo_routePartialArrive'] span");
+      const arriveTime = arriveSpans[0]?.querySelector('div span')?.textContent.trim() || '';
+      // Usa o seletor correto para o código do aeroporto
+      const arriveDiv = ticket.querySelector("div[class^='LegInfo_routePartialArrive'] div[aria-label]");
+
+      const arriveCode = arriveDiv?.getAttribute('aria-label') || '';
+      // Extrai informações de paradas
+      const stops = ticket.querySelector("div[class^='LegInfo_stopsLabelContainer']")?.textContent.trim() || '';
+      const price = ticket.querySelector("div[class^='Price_mainPriceContainer'] span")?.textContent.trim() || '';
+
+      return { departTime, departCode, stops, arriveTime, arriveCode, price };
+    });
+  });
+
+  console.log('Informações extraídas:', flightInfo);
+
+  // Fechar o navegador após a interação
+  await browser.close();
+}
   
 
-function buildUrl(origem, destino, dataIda, dataVolta) {
-    return `https://www.skyscanner.com.br/transporte/passagens-aereas/${origem}/${destino}/${dataIda}/${dataVolta}?adultsv2=1&cabinclass=economy&childrenv2=&inboundaltsenabled=false&outboundaltsenabled=false&preferdirects=false&ref=home&rtn=0`;
-}
 
-module.exports = { getRoundTripData, buildUrl };
+module.exports = { getRoundTripData, getTripData };
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 // main.js
-const { getRoundTripData, buildUrl } = require('./func/scrapper'); // Ajuste o caminho do arquivo conforme necessário
+const { getRoundTripData, getTripData } = require('./func/scrapper'); // Ajuste o caminho do arquivo conforme necessário
 
 const origem = 'NAT';
 const destino = 'VVI';
 const dataIda = '240109';
 const dataVolta = '240122';
 
-const url = buildUrl(origem, destino, dataIda, dataVolta);
-
-getRoundTripData(url).catch(err => console.error(err));
+//getRoundTripData(origem, destino, dataIda, dataVolta).catch(err => console.error(err));
+getTripData(origem, destino, dataIda).catch(err => console.error(err));
