7193d13d86a9df0c75ef1218253feb4ec33a08a4
allefschmidt <dev.allefschmidt@gmail.com>
2023-11-06 09:06:07 -0300

    :sparkles: criada função bestPrices

--- func/scrapper.js
+++ func/scrapper.js
@@ -101,7 +101,61 @@ async function getTripData(origem, destino, dataIda) {
   // Fechar o navegador após a interação
   await browser.close();
 }
-  
+
+async function bestPrices(origem, dataIda, pais) {
+  const browser = await puppeteer.launch({
+    headless: false, // Abre o navegador em modo não headless para interação manual
+  });
+
+  const url = `https://www.skyscanner.com.br/transporte/passagens-aereas/${origem}/${pais}/${dataIda}/?adultsv2=1&cabinclass=economy&childrenv2=&ref=home&rtn=0&preferdirects=false&outboundaltsenabled=false&inboundaltsenabled=false`
+  const page = await browser.newPage();
+
+  // Navega para a URL desejada
+  await page.goto(url, { waitUntil: 'networkidle2' });
+  await page.waitForTimeout(5000);
+
+  // Aguarda a div CombinedResultsPlaces_container ser carregada
+  await page.waitForSelector("div[class*='CombinedResultsPlaces_container']", { visible: true });
+
+  // Captura as informações específicas das divs BpkCard_bpk-card e ResultList_placeCard
+  const placeCardsContent = await page.evaluate(() => {
+    // Seleciona a div CombinedResultsPlaces_container
+    const combinedResultsContainer = document.querySelector("div[class*='CombinedResultsPlaces_container']");
+
+    // Dentro dessa div, seleciona todas as divs com a classe ResultList_placeCard
+    const placeCards = Array.from(combinedResultsContainer.querySelectorAll("div[class*='BpkCard_bpk-card'][class*='ResultList_placeCard']"));
+
+    // Mapeia cada div para extrair as informações necessárias
+    return placeCards.map(card => {
+      const linkElement = card.querySelector('a');
+      const imageElement = card.querySelector('div[class*="BpkBackgroundImage_bpk-background-image"] div[class*="BpkBackgroundImage_bpk-background-image__img"]');
+      const descriptionElement = card.querySelector('div[class*="PlaceCard_descriptionContainer"]');
+
+      // Extrai o URL da imagem de estilo inline
+      const imageStyle = imageElement.style.backgroundImage;
+      const imageUrlMatch = imageStyle.match(/url\("(.+?)"\)/);
+      const imageUrl = imageUrlMatch ? imageUrlMatch[1] : '';
+
+      // Extrai os textos
+      const texts = Array.from(descriptionElement.querySelectorAll('span[class*="BpkText_bpk-text"]')).map(span => span.textContent);
+
+      return {
+       // link: linkElement ? linkElement.href : '',
+       // imageUrl: imageUrl,
+        oportunidade: texts
+      };
+    });
+  });
+
+  console.log('Conteúdo das place cards:', placeCardsContent);
+
+  // Fechar o navegador após a interação
+  await browser.close();
+}
 
 
-module.exports = { getRoundTripData, getTripData };
+
+
+
+
+module.exports = { getRoundTripData, getTripData, bestPrices };
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // main.js
-const { getRoundTripData, getTripData } = require('./func/scrapper'); // Ajuste o caminho do arquivo conforme necessário
+const { getRoundTripData, getTripData, bestPrices } = require('./func/scrapper'); // Ajuste o caminho do arquivo conforme necessário
 
-const origem = 'NAT';
-const destino = 'VVI';
-const dataIda = '240109';
+const pais = 'BR'
+const origem = 'REC';
+const destino = 'MAO';
+const dataIda = '240106';
 const dataVolta = '240122';
 
 //getRoundTripData(origem, destino, dataIda, dataVolta).catch(err => console.error(err));
-getTripData(origem, destino, dataIda).catch(err => console.error(err));
+//getTripData(origem, destino, dataIda).catch(err => console.error(err));
+bestPrices(origem, dataIda, pais).catch(err => console.error(err));
